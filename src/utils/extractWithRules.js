772aac2d295ebf13d0288591d0a82ce6aaeb5ad0
Dong Nguyen <ndaidong@gmail.com>
2021-02-06 23:37:23 +0700

    Fix problem with defence-blog.com
    Related issue: #148

--- src/utils/extractWithRules.js
+++ src/utils/extractWithRules.js
@@ -25,6 +25,7 @@ module.exports = (html) => {
     });
 
     const potentialClasses = [
+      '.tdb-block-inner', // defence-blog.com
       '.post-full-content',
       '.rich-text',
       '.post-content',
