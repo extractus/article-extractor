3172f1336cdbdb54ef9aa7f93af0e542f84688f9
Dong Nguyen <ndaidong@gmail.com>
2021-12-02 21:56:02 +0700

    Change default user-agent string to Firefox

--- src/config.js
+++ src/config.js
@@ -2,13 +2,11 @@
 
 const { clone, copies } = require('bellajs')
 
-const { name, version } = require('../package.json')
-
 const env = process.env
 
 const fetchOptions = {
   headers: {
-    'user-agent': `${name}/${version}`
+    'user-agent': 'Mozilla/5.0 (X11; Linux i686; rv:94.0) Gecko/20100101 Firefox/94.0'
   },
   timeout: 30 * 1e3,
   redirect: 'follow'
