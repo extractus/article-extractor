2eab800fe176c69e7ee90bb125ae3e176f2b6c74
SettingDust <settingdust@gmail.com>
2022-02-18 18:08:58 +0800

    chore: lint

--- src/utils/extractWithReadability.test.js
+++ src/utils/extractWithReadability.test.js
@@ -9,7 +9,7 @@ import extractWithReadability from './extractWithReadability.js'
 
 test('test extractWithReadability from good html content', async () => {
   const html = readFileSync('./test-data/regular-article.html', 'utf8')
-  const result = extractWithReadability(html, "https://foo.bar")
+  const result = extractWithReadability(html, 'https://foo.bar')
   expect(isString(result)).toBe(true)
   expect(result.length > 200).toBe(true)
   expect(result).toEqual(expect.stringContaining('<img src="https://foo.bar/orange.png">'))
