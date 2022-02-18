a35f93c7ce85556fe21d347f6d39a3fbad4b9bc5
SettingDust <settingdust@gmail.com>
2022-02-18 18:46:51 +0800

    feat: update for https://github.com/WebReflection/linkedom/issues/117

--- src/utils/extractWithReadability.js
+++ src/utils/extractWithReadability.js
@@ -7,13 +7,10 @@ import { isString } from 'bellajs'
 export default (html, inputUrl) => {
   if (!isString(html)) return null
   const doc = new DOMParser().parseFromString(html, 'text/html')
-  try {
-    doc.baseURI = inputUrl
-  } catch (e) {
-    const base = doc.createElement('base')
-    base.href = inputUrl
-    doc.head.appendChild(base)
-  }
+  const base = doc.createElement('base')
+  base.setAttribute('href', inputUrl)
+  doc.head.appendChild(base)
+  console.log(doc.baseURI)
   const reader = new Readability(doc)
   const result = reader.parse() || {}
   const { content, textContent, length } = result
