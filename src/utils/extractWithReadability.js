bcb028632f897e6a2324f1fedb2e920566bca7e3
SettingDust <settingdust@gmail.com>
2022-02-18 18:05:25 +0800

    fix: linkedom difference with browser... add relative image test

--- src/utils/extractWithReadability.js
+++ src/utils/extractWithReadability.js
@@ -4,12 +4,16 @@ import { Readability } from '@mozilla/readability'
 import { DOMParser } from 'linkedom'
 import { isString } from 'bellajs'
 
-export default (html, url) => {
+export default (html, inputUrl) => {
   if (!isString(html)) return null
   const doc = new DOMParser().parseFromString(html, 'text/html')
-  const base = doc.createElement('base')
-  base.href = url
-  doc.head.appendChild(base)
+  try {
+    doc.baseURI = inputUrl
+  } catch (e) {
+    const base = doc.createElement('base')
+    base.href = inputUrl
+    doc.head.appendChild(base)
+  }
   const reader = new Readability(doc)
   const result = reader.parse() || {}
   const { content, textContent, length } = result
