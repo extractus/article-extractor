5df46fc5ce1f3bcc27f1b9c9e7174a84bd184bc8
Dave Schumaker <daveschumaker@users.noreply.github.com>
2022-04-01 13:54:09 -0700

    chore: create new methods split from standardizeArticle

---
+++ src/utils/normalizeUrls.js
@@ -0,0 +1,31 @@
+// utils -> normalizeUrls
+
+import absolutifyUrl from './absolutifyUrl.js'
+
+import { DOMParser } from 'linkedom'
+
+/**
+ * @param inputHtml {string}
+ * @param url {string}
+ * @returns {Promise<string>}
+ */
+export default (inputHtml, url) => {
+  const $article = new DOMParser().parseFromString(inputHtml, 'text/html')
+
+  Array.from($article.getElementsByTagName('a')).forEach((node) => {
+    const href = node.getAttribute('href')
+    if (href) {
+      node.setAttribute('href', absolutifyUrl(url, href))
+      node.setAttribute('target', '_blank')
+    }
+  })
+
+  Array.from($article.getElementsByTagName('img')).forEach((node) => {
+    const src = node.getAttribute('data-src') ?? node.getAttribute('src')
+    if (src) {
+      node.setAttribute('src', absolutifyUrl(url, src))
+    }
+  })
+
+  return $article.toString()
+}
---
+++ src/utils/transformHtml.js
@@ -0,0 +1,20 @@
+// utils -> transformHtml
+
+import { DOMParser } from 'linkedom'
+
+/**
+ * @param inputHtml {string}
+ * @param transform {(Document)=>Document}
+ * @returns {Promise<string>}
+ */
+export default (inputHtml, transform = null) => {
+  if (!transform) {
+    return inputHtml
+  }
+
+  const $article = new DOMParser().parseFromString(inputHtml, 'text/html')
+
+  const document = transform($article)
+
+  return document.toString()
+}
