f6e3ffdc4114f8fe2dca8a351f61ad1585099602
Dong Nguyen <ndaidong@gmail.com>
2022-07-09 17:55:31 +0700

    v7.0.0rc2

--- src/utils/extractWithReadability.js
+++ src/utils/extractWithReadability.js
@@ -4,7 +4,6 @@ import { Readability } from '@mozilla/readability'
 import { DOMParser } from 'linkedom'
 
 import { isValid as isHTMLString } from './html.js'
-import { error } from './logger.js'
 
 /**
  * @param html {string}
@@ -12,30 +11,20 @@ import { error } from './logger.js'
  * @returns {string|null}
  */
 export default (html, inputUrl = '') => {
-  try {
-    if (!isHTMLString(html)) return null
-    const doc = new DOMParser().parseFromString(html, 'text/html')
-    const base = doc.createElement('base')
-    base.setAttribute('href', inputUrl)
-    doc.head.appendChild(base)
-    const reader = new Readability(doc)
-    const result = reader.parse() ?? {}
-    return result.textContent ? result.content : null
-  } catch (err) {
-    error(err.message)
-    return null
-  }
+  if (!isHTMLString(html)) return null
+  const doc = new DOMParser().parseFromString(html, 'text/html')
+  const base = doc.createElement('base')
+  base.setAttribute('href', inputUrl)
+  doc.head.appendChild(base)
+  const reader = new Readability(doc)
+  const result = reader.parse() ?? {}
+  return result.textContent ? result.content : null
 }
 
 export function extractTitleWithReadability (html) {
-  try {
-    if (!isHTMLString(html)) return null
-    const doc = new DOMParser().parseFromString(html, 'text/html')
-    const reader = new Readability(doc)
-    // noinspection JSUnresolvedFunction
-    return reader._getArticleTitle()
-  } catch (err) {
-    error(err.message)
-    return ''
-  }
+  if (!isHTMLString(html)) return null
+  const doc = new DOMParser().parseFromString(html, 'text/html')
+  const reader = new Readability(doc)
+  // noinspection JSUnresolvedFunction
+  return reader._getArticleTitle()
 }
