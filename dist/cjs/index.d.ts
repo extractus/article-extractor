1151830598c72120b5ab1992649fa3ba85e41680
Dong Nguyen <ndaidong@gmail.com>
2022-09-03 10:56:29 +0700

    v7.0.2
    - Update dependencies
    - Add button "Deploy to Deta"
    - Use Deta service for example faas
    - Copy types definition to cjs dist (#287)

---
+++ dist/cjs/index.d.ts
@@ -0,0 +1,92 @@
+// Type definitions
+
+import {AxiosRequestConfig} from "axios";
+import {IOptions as SanitizeOptions} from "sanitize-html";
+import {defaults} from "html-crush";
+import "urlpattern-polyfill";
+
+type HtmlCrushOptions = Partial<typeof defaults>
+
+/**
+ * @example
+ * {
+ *   patterns: [
+ *     '*://example.com/books/:id', {
+ *       hostname: 'example.com',
+ *       pathname: '/books/:id',
+ *     }
+ *   ],
+ *   selector: '.article-body',
+ *   unwanted: ['.removing-box']
+ * }
+ */
+export interface Transformation {
+  patterns: Array<URLPatternInit | string>,
+  pre?: (document: Document) => Document
+  post?: (document: Document) => Document
+}
+
+/**
+ * @param input url or html
+ */
+export function extract(input: string): Promise<ArticleData>;
+
+export function addTransformations(transformations: Array<Transformation>): Number;
+
+export function removeTransformations(options: Array<URLPatternInit>): Number;
+
+export function setParserOptions(options: ParserOptions): void;
+
+export function setRequestOptions(options: AxiosRequestConfig): void;
+
+export function setSanitizeHtmlOptions(options: SanitizeOptions): void;
+
+export function setHtmlCrushOptions(options: HtmlCrushOptions): void;
+
+export function getParserOptions(): ParserOptions;
+
+export function getRequestOptions(): AxiosRequestConfig;
+
+export function getSanitizeHtmlOptions(): SanitizeOptions;
+
+export function getHtmlCrushOptions(): HtmlCrushOptions;
+
+export interface ParserOptions {
+  /**
+   * For estimating "time to read".
+   * Default: 300
+   */
+  wordsPerMinute: number
+  /**
+   * To find the best url from list
+   */
+  urlsCompareAlgorithm: 'levenshtein' | 'cosine' | 'diceCoefficient' | 'jaccardIndex' | 'lcs' | 'mlcs'
+  /**
+   * Min num of chars required for description
+   * Default: 40
+   */
+  descriptionLengthThreshold: number
+  /**
+   * Max num of chars generated for description
+   * Default: 156
+   */
+  descriptionTruncateLen: number
+  /**
+   * Min num of chars required for content
+   * Default: 200
+   */
+  contentLengthThreshold: number
+}
+
+export interface ArticleData {
+  url?: string;
+  links?: string[];
+  title?: string;
+  description?: string;
+  image?: string;
+  author?: string;
+  content?: string;
+  source?: string;
+  published?: string;
+  ttr?: number;
+}
